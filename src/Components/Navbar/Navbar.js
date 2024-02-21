e1fea48896a4ecde87cfbf48da95efd1b4d7b83d
kevin11paul <kevin11paul@gmail.com>
2024-02-21 15:51:39 +0530

    netflixclone

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -4,7 +4,7 @@ import "./Navbar.css";
 function Navbar() {
   return (
     <div className='navbar'>
-        <img className='logo' src="https://static.vecteezy.com/system/resources/previews/020/335/987/non_2x/netflix-logo-netflix-icon-free-free-vector.jpg"      alt="Netflix logo" />
+        <img className='logo' src="https://images.ctfassets.net/y2ske730sjqp/1aONibCke6niZhgPxuiilC/2c401b05a07288746ddf3bd3943fbc76/BrandAssets_Logos_01-Wordmark.jpg?w=940"      alt="Netflix logo" />
         <img className='avatar' src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png?20201013161117" alt="Avatar" />
     </div>
   )
